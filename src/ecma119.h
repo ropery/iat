f05f94237a2af333322794b90d29cca357b4fbc4
Salvatore Santagati <Salvatore.Santagati@gmail.com>
2009-08-11 10:49:36 +0200

    update license

--- src/ecma119.h
+++ src/ecma119.h
@@ -1,20 +1,52 @@
-/*  Copyright (C) 2008 Salvatore Santagati <salvatore.santagati@gmail.com>
+/**
+ * Copyright (C) 2008 
+ *	- Salvatore Santagati <salvatore.santagati@gmail.com>
+ * Copyright (C) 2009
+ * 	- Salvatore Santagati  <salvatore.santagati@gmail.com>
+ *      - Abdur Rab <c.abdur@yahoo.com>
+ *
+ * All rights reserved.
+ *
+ * This program is free software; under the terms of the
+ * GNU General Public License as published by the Free Software Foundation;
+ * either version 2 of the License, or (at your option) any later version.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * @ Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * @ Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+#ifndef ECMA119_H
+#define ECMA119_H
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the
-    Free Software Foundation, Inc.,
-    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*/
+#ifdef __cplusplus
+extern "C" {
+#endif  /* __cplusplus */
 
 typedef unsigned char		iso711_t;
 typedef	signed	 char		iso712_t;	
@@ -297,4 +329,8 @@ struct iso_ext_attr_record {
 #define ISO_RL_MAX_2	2
 #define ISO_RL_MAX_3	3
 
- 
+#ifdef __cplusplus
+}       /* extern "C" */
+#endif  /* __cplusplus */
+
+#endif /* ECMA119 */ 
