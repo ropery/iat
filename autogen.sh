e132c0638eb97e87e188655d62cb51a0551c993a
Salvatore Santagati <Salvatore.Santagati@gmail.com>
2009-08-11 10:45:39 +0200

    update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gengetopt  --output-dir=src < iat.ggo 
+#gengetopt  --output-dir=src < iat.ggo 
 autoreconf --force
-./configure
-make
-./help2man.pl -P iat -I iat.h2m  src/iat > man/iat.1
+./configure --enable-maintainer-mode
+#make
+#./help2man.pl -N -P iat -I iat.h2m src/iat > man/iat.1
