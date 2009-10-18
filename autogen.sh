b19ac9008c77623d7f7cb6aae5946836070cd3e7
Salvatore Santagati <Salvatore.Santagati@gmail.com>
2009-10-18 09:36:04 +0200

    automate version of program

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+
+# SET configure.ac
+
+# version
+GITVERSION=`date  +%Y%m%d%H%M`
+GITDATEVERSION=GITDATEVERSION
+# file name
+CONFIGURE_AC=configure.ac
+# edit configure.ac
+sed -i  "s/git.*.dev,/git.$GITVERSION.dev,/" $CONFIGURE_AC 
+echo "configure.ac : OK";
+
 #gengetopt  --output-dir=src < iat.ggo 
 autoreconf --force
+echo "autoreconf : OK";
 #./configure --enable-maintainer-mode
 #make
 #./help2man.pl -N -P iat -I iat.h2m src/iat > man/iat.1
